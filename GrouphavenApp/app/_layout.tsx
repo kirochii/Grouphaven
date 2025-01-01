e33569229dbd528ffec65e11b0c59c51d72c585e
Ho Zhi Xuen <xuen1156@gmail.com>
2025-01-01 16:03:50 +0800

    Finished index page

--- GrouphavenApp/app/_layout.tsx
+++ GrouphavenApp/app/_layout.tsx
@@ -1,5 +1,35 @@
 import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded, error] = useFonts({
+    'Inter-Regular': require('../assets/fonts/Inter_28pt-Regular.ttf'),
+    'Inter-Bold': require('../assets/fonts/Inter_28pt-Bold.ttf'),
+    'Inter-Black': require('../assets/fonts/Inter_28pt-Black.ttf'),
+    'Inter-ExtraBold': require('../assets/fonts/Inter_28pt-ExtraBold.ttf'),
+    'Inter-ExtraLight': require('../assets/fonts/Inter_28pt-ExtraLight.ttf'),
+    'Inter-Light': require('../assets/fonts/Inter_28pt-Light.ttf'),
+    'Inter-Medium': require('../assets/fonts/Inter_28pt-Medium.ttf'),
+    'Inter-SemiBold': require('../assets/fonts/Inter_28pt-SemiBold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
--- GrouphavenApp/app/index.tsx
+++ GrouphavenApp/app/index.tsx
@@ -1,8 +1,8 @@
 import { StyleSheet, ImageBackground, View, Image } from 'react-native';
-import { Provider as PaperProvider, Text } from 'react-native-paper';
+import { Provider as PaperProvider, Text, Button } from 'react-native-paper';
 import React from 'react';
 
-export default function HomeScreen() {
+export default function LandingScreen() {
   return (
     <PaperProvider>
       <ImageBackground
@@ -11,14 +11,49 @@ export default function HomeScreen() {
         style={styles.background}
       >
         <View style={styles.container}>
-          <View style={styles.iconContainer}>
-            <Image
-              source={require('../assets/images/iconWhite.png')}  // Replace with your image path
-              style={styles.icon}
-            />
-            <Text style={styles.iconText}>GROUPHAVEN</Text>
+          <View style={styles.headerContainer}>
+            <View style={styles.iconContainer}>
+              <Image
+                source={require('../assets/images/iconWhite.png')}
+                style={styles.icon}
+              />
+              <Text style={styles.iconText}>GROUPHAVEN</Text>
+            </View>
+            <Text style={styles.text}>Match. Connect. Thrive.</Text>
           </View>
-          <Text style={styles.text}>Match. Connect. Thrive.</Text>
+
+          <View style={styles.buttonContainer}>
+            <Button style={styles.button} labelStyle={styles.buttonText} icon={() => (
+              <Image
+                source={require('../assets/images/logo_Facebook.png')}
+                style={styles.iconImage}
+              />
+            )} mode="elevated" rippleColor="rgba(0, 0, 0, 0.2)" onPress={() => console.log('Pressed')}>
+              Continue with Facebook
+            </Button>
+            <Button style={styles.button} labelStyle={styles.buttonText} icon={() => (
+              <Image
+                source={require('../assets/images/logo_Google.png')}
+                style={styles.iconImage}
+              />
+            )} mode="elevated" rippleColor="rgba(0, 0, 0, 0.2)" onPress={() => console.log('Pressed')}>
+              Continue with Google
+            </Button>
+            <Button style={styles.button} labelStyle={styles.buttonText} icon={() => (
+              <Image
+                source={require('../assets/images/logo_X.png')}
+                style={styles.iconImage}
+              />
+            )} mode="elevated" rippleColor="rgba(0, 0, 0, 0.2)" onPress={() => console.log('Pressed')}>
+              Continue with X
+            </Button>
+          </View>
+
+          <Text style={styles.termText}>
+            By continuing, you agree to our{' '}
+            <Text style={styles.link}>Terms of Service</Text> and acknowledge that you have read our{' '}
+            <Text style={styles.link}>Privacy Policy</Text>.
+          </Text>
         </View>
       </ImageBackground>
     </PaperProvider>
@@ -32,31 +67,68 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    padding: 20,
+    padding: '5%',
+  },
+  headerContainer: {
+    flex: 7,
+    alignItems: 'center',
+    paddingTop: '15%',
   },
   text: {
-    marginBottom: 20,
-    fontSize: 24,
+    fontFamily: 'Inter-Light',
+    fontSize: 22,
     color: 'white',
-    fontWeight: 'bold',
+    paddingTop: '5%',
   },
   iconContainer: {
-    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 75,
+    alignItems: 'center',
   },
   icon: {
-    width: 72,
-    height: 72,
+    width: '20%',
+    aspectRatio: 1,
+    resizeMode: 'contain',
   },
   iconText: {
-    marginTop: 20,
-    marginLeft: 10,
-    fontSize: 24,
+    fontFamily: 'Inter-Black',
+    marginLeft: '3%',
+    fontSize: 32,
     color: 'white',
-    fontWeight: 'bold',
-  }
+  },
+  link: {
+    fontFamily: 'Inter-SemiBold',
+    textDecorationLine: 'underline',
+    color: 'white',
+  },
+  termText: {
+    flex: 1,
+    fontFamily: 'Inter-Light',
+    fontSize: 14,
+    color: 'white',
+    textAlign: 'center',
+    width: '90%',
+    paddingTop: '15%',
+  },
+  buttonContainer: {
+    flex: 3,
+    justifyContent: 'space-between',
+    width: '100%',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: 'white',
+    borderRadius: 15,
+    width: '90%',
+  },
+  buttonText: {
+    fontFamily: 'Inter-Bold',
+    fontSize: 16,
+    color: 'black',
+  },
+  iconImage: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+  },
 });
